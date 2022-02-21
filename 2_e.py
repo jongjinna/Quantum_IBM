4e0fd952e94d632381436181de799193a7939b4a
jongjinna <njjwa1004@naver.com>
2022-02-21 23:47:20 +0900

    :memo: make quantum program

--- 2_e.py
+++ 2_e.py
@@ -1,1 +1,25 @@
-print("hello world")
+from matplotlib import backend_bases
+import numpy as np
+from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
+from qiskit import execute
+from qiskit import BasicAer
+from qiskit.tools.visualization import plot_state_city
+
+q = QuantumRegister(2, 'q')
+circuit = QuantumCircuit(q)
+circuit.h(0)
+circuit.cx(0, 1)
+
+print(circuit.draw())
+
+backend = BasicAer.get_backend('statevector_simulator')
+
+job = execute(circuit, backend)
+
+result = job.result()
+
+outputstate = result.get_statevector(circuit, decimals=2)
+
+print(outputstate)
+
+plot_state_city(outputstate)
