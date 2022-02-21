ccf8fcb325c0222f2420e3d62879a1ff9bef48a6
jongjinna <njjwa1004@naver.com>
2022-02-21 19:15:15 +0900

    :tada: installing qiskit & start studying

--- 1_e.py
+++ 1_e.py
@@ -6,4 +6,4 @@ from qiskit import IBMQ
 
 IBMQ.save_account('8336b11cda16a83758d674d076091ebbeaf397c771a335a694fa2e048f5cda647be15a1c17c06eefad60630f19ab8ecb75150ca72ee029af7f5db79b170082cd')
 
-print(IBMQ.load_account())
+IBMQ.load_account()
